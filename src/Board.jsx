9ad914efd8be1f9d3475308f516b3718e2085cf9
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-05-04 19:44:53 +0300

    Place calculateWinner func in utils file

--- src/Board.jsx
+++ src/Board.jsx
@@ -1,4 +1,5 @@
 import Square from './Square.jsx';
+import { calculateWinner } from './utils.js';
 
 export default function Board({ xIsNext, squares, onPlay }) {
   function handleClick(i) {
@@ -47,25 +48,4 @@ function createBoard(rowN, colN, squares, handleClick, lines=[]) {
     board.push(<div key={r} className='board-row'>{cols}</div>);
   }
   return board;
-}
-
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return { lines: lines[i], symbol: squares[a] };
-    }
-  }
-  return null;
 }
