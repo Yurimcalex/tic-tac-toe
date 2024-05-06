34712fd15c17b74e40eb4aa3f245fccd2ad06791
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-05-06 17:35:41 +0300

    Update the Controls comp to show cells amount need to fill to win

--- src/Controls.jsx
+++ src/Controls.jsx
@@ -1,6 +1,7 @@
 export default function Controls({ rows, cols, changeRows, changeCols }) {
 	return (
 		<div className='controls'>
+			<p>Fill {Math.min(rows, cols)} neighboring cells to win</p>
 			<div>
 				<span>rows:</span>
 				<input type="number" min="3" value={rows} onChange={changeRows} />
