d4d8848632caf561a3b6da9854b68a65bcf7c678
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-05-06 16:07:25 +0300

    Fix createBoard func to highlight and to shade cells correctly

--- src/Board.jsx
+++ src/Board.jsx
@@ -23,26 +23,34 @@ export default function Board({ xIsNext, squares, onPlay, rows, cols }) {
   return (
     <>
       <Status xIsNext={xIsNext} winner={winner} squares={squares} />
-      <div>{createBoard(rows, cols, squares, handleClick, winner?.lines)}</div>
+      <div>{createBoard(rows, cols, squares, handleClick, winner)}</div>
     </>
   );
 }
 
 
-function createBoard(rowN, colN, squares, handleClick, lines=[]) {
+function createBoard(rowN, colN, squares, handleClick, winner) {
   const board = [];
   let counter = 0;
   for (let r = 0; r < rowN; r += 1) {
     let cols = [];
     for (let c = 0; c < colN; c += 1) {
-      let ind = counter;
+      const ind = counter;
+      let highlight = false;
+      let shade = false;
+
+      if (winner) {
+        highlight = winner.lines.includes(ind) && squares[ind] === winner.symbol;
+        shade = !highlight;
+      }
+
       cols.push(
         <Square 
           key={c}
           value={squares[ind]}
           onSquareClick={() => handleClick(ind)}
-          highlight={lines.includes(ind)}
-          shade={lines.length && !lines.includes(ind)} />
+          highlight={highlight}
+          shade={shade} />
       );
       counter += 1;
     }
